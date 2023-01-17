92ddae978f75cdad625a60376e5f9f1d95d1d052
clarafy <clarafy@berkeley.edu>
2023-01-17 15:48:28 -0800

    alphafold iid clt results

--- alphafold/utils.py
+++ alphafold/utils.py
@@ -3,6 +3,7 @@ sys.path.insert(1, '../')
 from concentration import wsr_iid
 
 import numpy as np
+import scipy.stats as stats
 import math
 import multiprocess
 
@@ -130,23 +131,27 @@ def get_betting_wor_ci(x_n, N, alpha, grid_spacing, use_intersection: bool = Tru
     return np.array([np.maximum(l, l_logical), np.minimum(u, u_logical)])
 
 
-# odds ratio estimation based on finite sample mean estimation
-def get_odds_ratio_cis(df, ptm_name, n, alpha, grid_spacing: float = 1e-3,
-                       use_iid_approximation: bool = True, use_intersection: bool = True, verbose: bool = True,
-                       parallelize: bool = False, n_cores: int = None, n_min: int = 20):
+# TODO for IID: fix WSR version to not double dip labeled and split error budget,
+# implement CLT version where do not split error budget
+def get_odds_ratio_intervals(df, ptm_name, n, alpha, delta_frac: float = 0.1, use_clt: bool = False,
+                             grid_spacing: float = 1e-3, use_iid_approximation: bool = True,
+                             use_intersection: bool = True, verbose: bool = True,
+                             parallelize: bool = False, n_cores: int = None, n_min: int = 20):
 
     # Z: df[ptm_name]
     # Y: df['disordered'], binary
     # f: df['pred_disordered'], in [0, 1]
 
-    # ===== partition n labels into Z = 1 and Z = 0 =====
+    # ===== partition n labels between examples with Z = 1 and Z = 0 =====
 
+    # extract modified and unmodified instances from gold-standard Z
     z1_df = df.loc[df[ptm_name] == 1]
     z0_df = df.loc[df[ptm_name] == 0]
     N_z1 = len(z1_df)
     N_z0 = len(z0_df)
     assert(N_z1 + N_z0 == len(df))
 
+    # split budget of n labels between the two groups
     n_z1 = int(N_z1 * n / len(df))
     n_z0 = n - n_z1
     n_min = np.min([n_min, N_z1, N_z0])
@@ -168,61 +173,103 @@ def get_odds_ratio_cis(df, ptm_name, n, alpha, grid_spacing: float = 1e-3,
     z0_lab_idx = np.random.choice(N_z0, n_z0, replace=False)
     lab_z0_df = z0_df.iloc[z0_lab_idx]
 
+    # only used for IID approximation (finite population version will just take all of z1_df)
+    unlab_z1_df = z1_df.iloc[np.delete(np.arange(N_z1), z1_lab_idx)]
+    unlab_z0_df = z0_df.iloc[np.delete(np.arange(N_z0), z0_lab_idx)]
+
     # ===== CIs on mu1 = P(Y = 1 | Z = 1) and mu0 = P(Y = 1 | Z = 0) =====
 
     # ----- prediction-powered CI -----
 
-    bias_lab_z1_n = lab_z1_df['disordered'].to_numpy() - lab_z1_df['pred_disordered'].to_numpy()
-    bias_lab_z0_n = lab_z0_df['disordered'].to_numpy() - lab_z0_df['pred_disordered'].to_numpy()
+    rect_lab_z1_n = lab_z1_df['pred_disordered'].to_numpy() - lab_z1_df['disordered'].to_numpy()
+    rect_lab_z0_n = lab_z0_df['pred_disordered'].to_numpy() - lab_z0_df['disordered'].to_numpy()
+
     if use_iid_approximation:
-        grid = np.arange(grid_spacing, 1, step=grid_spacing)
-        # biases have values in {-1, 0, 1}, rescale to [0, 1]
-        bias_z1_ci = wsr_iid((bias_lab_z1_n + 1) / 2, alpha / 2, grid, parallelize=parallelize, num_cpus=n_cores)
-        bias_z0_ci = wsr_iid((bias_lab_z0_n + 1) / 2, alpha / 2, grid, parallelize=parallelize, num_cpus=n_cores)
-    else:
-        bias_z1_ci = get_betting_wor_ci(
-            (bias_lab_z1_n + 1) / 2, N_z1, alpha / 2, grid_spacing, use_intersection=use_intersection,
+        if use_clt:
+            f_unlab_z1_N = unlab_z1_df['pred_disordered'].to_numpy()
+            f_unlab_z0_N = unlab_z0_df['pred_disordered'].to_numpy()
+
+            # empirical variance of imputed estimate
+            sigma2f_z1 = np.mean(np.square(f_unlab_z1_N - f_unlab_z1_N.mean()))
+            sigma2f_z0 = np.mean(np.square(f_unlab_z0_N - f_unlab_z0_N.mean()))
+
+            # empirical variance of empirical rectifier
+            sigma2rect_z1 = np.mean(np.square(rect_lab_z1_n - rect_lab_z1_n.mean()))
+            sigma2rect_z0 = np.mean(np.square(rect_lab_z0_n - rect_lab_z0_n.mean()))
+
+            # CLT intervals
+            w_z1 = stats.norm.ppf(1 - alpha / 4) * np.sqrt(sigma2f_z1 / f_unlab_z1_N.size + sigma2rect_z1 / rect_lab_z1_n.size)
+            w_z0 = stats.norm.ppf(1 - alpha / 4) * np.sqrt(sigma2f_z0 / f_unlab_z0_N.size + sigma2rect_z0 / rect_lab_z0_n.size)
+            mu1_pp = f_unlab_z1_N.mean() - rect_lab_z1_n.mean()
+            mu0_pp = f_unlab_z0_N.mean() - rect_lab_z0_n.mean()
+            mu1_mai_ci = (np.maximum(mu1_pp - w_z1, 0), np.minimum(mu1_pp + w_z1, 1))
+            mu0_mai_ci = (np.maximum(mu0_pp - w_z0, 0), np.minimum(mu0_pp + w_z0, 1))
+
+        else:  # use finite-sample WSR concentration
+            grid = np.arange(grid_spacing, 1, step=grid_spacing)
+            delta = delta_frac * alpha
+            if verbose:
+                print('Using delta = {:.3f} for estimating big-N terms.'.format(delta))
+
+            # biases have values in [-1, 1], rescale to [0, 1]
+            rect_z1_ci = wsr_iid((rect_lab_z1_n + 1) / 2, (alpha - delta) / 2, grid, parallelize=False)
+            rect_z0_ci = wsr_iid((rect_lab_z0_n + 1) / 2, (alpha - delta) / 2, grid, parallelize=False)
+            rect_z1_ci = rect_z1_ci * 2 - 1  # rescale to [-1, 1]
+            rect_z0_ci = rect_z0_ci * 2 - 1
+
+            f_z1_ci = wsr_iid(unlab_z1_df['pred_disordered'].to_numpy(), delta / 2, grid, parallelize=False)
+            f_z0_ci = wsr_iid(unlab_z0_df['pred_disordered'].to_numpy(), delta / 2, grid, parallelize=False)
+
+            mu1_mai_ci = (np.maximum(f_z1_ci[0] - rect_z1_ci[1], 0), np.minimum(f_z1_ci[1] - rect_z1_ci[0], 1))
+            mu0_mai_ci = (np.maximum(f_z0_ci[0] - rect_z0_ci[1], 0), np.minimum(f_z0_ci[1] - rect_z0_ci[0], 1))
+
+    else: # finite population version
+        rect_z1_ci = get_betting_wor_ci(
+            (rect_lab_z1_n + 1) / 2, N_z1, alpha / 2, grid_spacing, use_intersection=use_intersection,
             parallelize=parallelize, n_cores=n_cores
         )
-        bias_z0_ci = get_betting_wor_ci(
-            (bias_lab_z0_n + 1) / 2, N_z0, alpha / 2, grid_spacing, use_intersection=use_intersection,
+        rect_z0_ci = get_betting_wor_ci(
+            (rect_lab_z0_n + 1) / 2, N_z0, alpha / 2, grid_spacing, use_intersection=use_intersection,
             parallelize=parallelize, n_cores=n_cores
         )
-    bias_z1_ci = bias_z1_ci * 2 - 1  # rescale to [-1, 1]
-    bias_z0_ci = bias_z0_ci * 2 - 1
-    f_mean_z1 = z1_df['pred_disordered'].mean()
-    f_mean_z0 = z0_df['pred_disordered'].mean()
-    mu1_mai_ci = (np.maximum(f_mean_z1 + bias_z1_ci[0], 0), np.minimum(f_mean_z1 + bias_z1_ci[1], 1))
-    mu0_mai_ci = (np.maximum(f_mean_z0 + bias_z0_ci[0], 0), np.minimum(f_mean_z0 + bias_z0_ci[1], 1))
-    if (mu1_mai_ci[1] < mu1_mai_ci[0]) or (mu0_mai_ci[1] < mu0_mai_ci[0]):
-        print(mu1_mai_ci)
-        print(mu0_mai_ci)
+        rect_z1_ci = rect_z1_ci * 2 - 1  # rescale to [-1, 1]
+        rect_z0_ci = rect_z0_ci * 2 - 1
+
+        f_z1 = z1_df['pred_disordered'].mean()
+        f_z0 = z0_df['pred_disordered'].mean()
+
+        mu1_mai_ci = (np.maximum(f_z1 - rect_z1_ci[1], 0), np.minimum(f_z1 - rect_z1_ci[0], 1))
+        mu0_mai_ci = (np.maximum(f_z0 - rect_z0_ci[1], 0), np.minimum(f_z0 - rect_z0_ci[0], 1))
 
     # ----- classical CI (using only labeled data) -----
+
     y_z1_n = lab_z1_df['disordered'].to_numpy()
     y_z0_n = lab_z0_df['disordered'].to_numpy()
     if use_iid_approximation:
-        mu1_cla_ci = wsr_iid(y_z1_n, alpha / 2, grid, parallelize=parallelize, num_cpus=n_cores)
-        mu0_cla_ci = wsr_iid(y_z0_n, alpha / 2, grid, parallelize=parallelize, num_cpus=n_cores)
+        if use_clt:
+            sigma2y_z1 = np.mean(np.square(y_z1_n - y_z1_n.mean()))
+            sigma2y_z0 = np.mean(np.square(y_z0_n - y_z0_n.mean()))
+            w_z1 = stats.norm.ppf(1 - alpha / 4) *  np.sqrt(sigma2y_z1 / y_z1_n.size)
+            w_z0 = stats.norm.ppf(1 - alpha / 4) *  np.sqrt(sigma2y_z0 / y_z0_n.size)
+            mu1_cla_ci = (np.maximum(y_z1_n.mean() - w_z1, 0), np.minimum(y_z1_n.mean() + w_z1, 1))
+            mu0_cla_ci = (np.maximum(y_z0_n.mean() - w_z0, 0), np.minimum(y_z0_n.mean() + w_z0, 1))
+        else: # use finite-sample WSR concentration
+            mu1_cla_ci = wsr_iid(y_z1_n, alpha / 2, grid, parallelize=False)
+            mu0_cla_ci = wsr_iid(y_z0_n, alpha / 2, grid, parallelize=False)
     else:
         mu1_cla_ci = get_betting_wor_ci(
-            y_z1_n, N_z1, alpha / 2, grid_spacing, use_intersection=use_intersection,
-            parallelize=parallelize, n_cores=n_cores
+            y_z1_n, N_z1, alpha / 2, grid_spacing, use_intersection=use_intersection, parallelize=False
         )
         mu0_cla_ci = get_betting_wor_ci(
-            y_z0_n, N_z0, alpha / 2, grid_spacing, use_intersection=use_intersection,
-            parallelize=parallelize, n_cores=n_cores
+            y_z0_n, N_z0, alpha / 2, grid_spacing, use_intersection=use_intersection, parallelize=False
         )
-    if (mu1_cla_ci[1] < mu1_cla_ci[0]) or (mu0_cla_ci[1] < mu0_cla_ci[0]):
-        print(mu1_cla_ci)
-        print(mu0_cla_ci)
 
     # ===== CIs on odds ratio =====
 
     o_mai_ci = get_odds_ratio_ci_from_mu_ci(mu1_mai_ci, mu0_mai_ci)
     o_cla_ci = get_odds_ratio_ci_from_mu_ci(mu1_cla_ci, mu0_cla_ci)
 
-    # ----- true quantities -----
+    # ----- true (finite population) quantities -----
     mu1 = z1_df['disordered'].mean()
     mu0 = z0_df['disordered'].mean()
     n_z1_y1 = len(df.loc[(df[ptm_name] == 1) & (df['disordered'] == 1)])
