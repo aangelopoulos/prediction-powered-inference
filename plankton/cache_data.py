2efd383f2b8e7a5b83984810980e5e26e0d1c30f
Anastasios N. Angelopoulos <angelopoulos@berkeley.edu>
2023-01-23 00:11:21 -0800

    [notebook edits]

--- plankton/cache_data.py
+++ plankton/cache_data.py
@@ -5,19 +5,19 @@ import torchvision as tv
 import matplotlib.pyplot as plt
 from collections import Counter
 from tqdm import tqdm
-from utils import get_test_dataset 
+from utils import get_test_dataset
 
 if __name__ == "__main__":
   # Get data 2006-2013 from the following link: https://darchive.mblwhoilibrary.org/handle/1912/7341
   # Unzip and merge the datasets in the following directory
-  model_data_dir = '/home/group/mai_datasets/plankton/merged-2006-2012'
-  calib_data_dir = '/home/group/mai_datasets/plankton/no-empties-2013'
-  test_data_dir = '/home/group/mai_datasets/plankton/2014'
-  binary = False 
+  model_data_dir = '/home/group/ppi_datasets/plankton/merged-2006-2012'
+  calib_data_dir = '/home/group/ppi_datasets/plankton/no-empties-2013'
+  test_data_dir = '/home/group/ppi_datasets/plankton/2014'
+  binary = False
 
   # Batch size for training (change depending on how much memory you have)
   batch_size = 512
-  
+
   # Create training, calibration, and test datasets
   train_dataset = get_test_dataset(model_data_dir, binary=binary)
   calib_dataset = get_test_dataset(calib_data_dir, binary=binary)
@@ -32,23 +32,23 @@ if __name__ == "__main__":
   print(f"The number of classes is {num_classes}")
   calib_dataloader = torch.utils.data.DataLoader(calib_dataset, collate_fn=collate_fn, batch_size=batch_size, shuffle=True, num_workers=0)
   test_dataloader = torch.utils.data.DataLoader(test_dataset, collate_fn=collate_fn, batch_size=batch_size, shuffle=False, num_workers=0)
-  calib_corr = torch.tensor([ train_dataset.class_to_idx[cls] for cls in calib_dataset.classes ]) 
-  test_corr = torch.tensor([ train_dataset.class_to_idx[cls] for cls in test_dataset.classes ]) 
-  
+  calib_corr = torch.tensor([ train_dataset.class_to_idx[cls] for cls in calib_dataset.classes ])
+  test_corr = torch.tensor([ train_dataset.class_to_idx[cls] for cls in test_dataset.classes ])
+
   # Detect if we have a GPU available
   device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
   print(f"Using device {device}")
-  
+
   model_ft = tv.models.resnet152(pretrained=True)
   num_ftrs = model_ft.fc.in_features
   model_ft.fc = torch.nn.Linear(num_ftrs, num_classes)
-  
+
   # Send the model to GPU
   model_ft = model_ft.to(device)
   model_ft.load_state_dict(torch.load('./models/model_v6.pth'))
   model_ft.eval()
-  
-  # Calculate outputs 
+
+  # Calculate outputs
   with torch.no_grad():
     count = 0
     test_labels = np.zeros((len(test_dataset),))
@@ -59,33 +59,33 @@ if __name__ == "__main__":
 
       for inputs, batch_labels in tqdm(test_dataloader):
         inputs = inputs.to(device)
-        test_labels[count:count+batch_labels.shape[0]] = test_corr[batch_labels].detach().numpy() 
-            
-        # Get model outputs 
+        test_labels[count:count+batch_labels.shape[0]] = test_corr[batch_labels].detach().numpy()
+
+        # Get model outputs
         outputs = model_ft(inputs)
-        
+
         _, batch_preds = torch.max(outputs, 1)
         test_preds[count:count+batch_labels.shape[0]] = batch_preds.detach().cpu().numpy()
         count += batch_labels.shape[0]
-  
+
       np.savez('./test-outputs.npz', preds=test_preds, labels=test_labels)
-  
+
     count = 0
     calib_labels = np.zeros((len(calib_dataset),))
     calib_preds = np.zeros((len(calib_dataset),))
-  
+
     if not os.path.exists('./calib-outputs.npz'):
       print("Caching the calib outputs.")
       for inputs, batch_labels in tqdm(calib_dataloader):
         inputs = inputs.to(device)
-        calib_labels[count:count+batch_labels.shape[0]] = calib_corr[batch_labels].detach().numpy() 
-            
-        # Get model outputs 
+        calib_labels[count:count+batch_labels.shape[0]] = calib_corr[batch_labels].detach().numpy()
+
+        # Get model outputs
         outputs = model_ft(inputs)
-        
+
         _, batch_preds = torch.max(outputs, 1)
         calib_preds[count:count+batch_labels.shape[0]] = batch_preds.detach().cpu().numpy()
         count += batch_labels.shape[0]
-  
+
       np.savez('./calib-outputs.npz', preds=calib_preds, labels=calib_labels)
 
--- plankton/train_model.py
+++ plankton/train_model.py
@@ -11,24 +11,24 @@ import pdb
 if __name__ == "__main__":
   # Get data 2006-2013 from the following link: https://darchive.mblwhoilibrary.org/handle/1912/7341
   # Unzip and merge the datasets in the following directory
-  data_dir = '/home/group/mai_datasets/plankton/merged-2006-2012'
-  
+  data_dir = '/home/group/ppi_datasets/plankton/merged-2006-2012'
+
   # Pct Val
   pct_val = 0.05
-  binary = False 
-  
+  binary = False
+
   # Batch size for training (change depending on how much memory you have)
-  batch_size = 32 
-  
+  batch_size = 32
+
   # Number of epochs to train for
-  num_epochs = 2 
-  
+  num_epochs = 2
+
   # Flag for feature extracting. When False, we finetune the whole model,
   #   when True we only update the reshaped layer params
-  feature_extract = False 
-  
+  feature_extract = False
+
   print("Initializing Datasets and Dataloaders...")
-  
+
   # Create training and validation datasets
   train_dataset, val_dataset = get_train_val_split(data_dir, pct_val, binary=binary)
 
@@ -42,25 +42,25 @@ if __name__ == "__main__":
   print(f"The number of classes is {num_classes}")
   train_dataloader = torch.utils.data.DataLoader(train_dataset, collate_fn=collate_fn, batch_size=batch_size, shuffle=True, num_workers=0)
   val_dataloader = torch.utils.data.DataLoader(val_dataset, collate_fn=collate_fn, batch_size=batch_size, shuffle=True, num_workers=0)
-  
+
   # Detect if we have a GPU available
   device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
   print(f"Using device {device}")
-  
+
   # Set up model
   def set_parameter_requires_grad(model, feature_extracting):
     if feature_extracting:
       for param in model.parameters():
         param.requires_grad = False
-  
+
   model_ft = tv.models.resnet152(pretrained=True)
   set_parameter_requires_grad(model_ft, feature_extract)
   num_ftrs = model_ft.fc.in_features
   model_ft.fc = torch.nn.Linear(num_ftrs, num_classes)
-  
+
   # Send the model to GPU
   model_ft = model_ft.to(device)
-  
+
   # Gather the parameters to be optimized/updated in this run. If we are
   #  finetuning we will be updating all parameters. However, if we are
   #  doing feature extract method, we will only update the parameters
@@ -78,13 +78,13 @@ if __name__ == "__main__":
     for name,param in model_ft.named_parameters():
       if param.requires_grad == True:
         print("\t",name)
-  
+
   # Observe that all parameters are being optimized
   optimizer_ft = torch.optim.AdamW(params_to_update, lr=1e-4)
-  
+
   # Setup the loss fxn
   criterion = torch.nn.CrossEntropyLoss()
-  
+
   # Train and evaluate
   model_ft, hist = train_model(model_ft, {'train': train_dataloader, 'val': val_dataloader}, criterion, optimizer_ft, num_epochs=num_epochs)
   save_checkpoint(model_ft.cpu(), hist)
